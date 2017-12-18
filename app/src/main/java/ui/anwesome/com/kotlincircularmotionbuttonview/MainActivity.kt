8092d209a6e528e9c58a06c846f0d5c39c2eb7ae
Anwesh43 <anweshthecool0@gmail.com>
2017-12-18 22:45:25 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlincircularmotionbuttonview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircularmotionbuttonview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincircularmotionbuttonview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circularmotionbuttonview.CircularMotionButtonView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircularMotionButtonView.create(this)
     }
 }
