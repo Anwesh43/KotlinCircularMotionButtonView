45b992db728ca1dc950074f0f5aed49f3c76d690
Anwesh43 <anweshthecool0@gmail.com>
2017-12-18 08:47:37 +0530

    using renderer in view

--- circularmotionbuttonview/src/main/java/ui/anwesome/com/circularmotionbuttonview/CircularMotionButtonView.kt
+++ circularmotionbuttonview/src/main/java/ui/anwesome/com/circularmotionbuttonview/CircularMotionButtonView.kt
@@ -10,12 +10,13 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class CircularMotionButtonView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = CircularMotionButtonRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         if(event.action == MotionEvent.ACTION_DOWN) {
-
+            renderer.handleTap(event.x,event.y)
         }
         return true
     }
