b769a476550009d3deeade978ee74dadb111a2d3
Anwesh43 <anweshthecool0@gmail.com>
2017-12-18 08:36:13 +0530

    using state

--- circularmotionbuttonview/src/main/java/ui/anwesome/com/circularmotionbuttonview/CircularMotionButtonView.kt
+++ circularmotionbuttonview/src/main/java/ui/anwesome/com/circularmotionbuttonview/CircularMotionButtonView.kt
@@ -28,30 +28,35 @@ class CircularMotionButtonView(ctx:Context):View(ctx) {
         fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
     data class CircularMotionButton(var i:Int = 0,var x:Float,var y:Float,var r:Float) {
+        val state = CircularMotionButtonState()
         fun draw(canvas:Canvas,paint:Paint) {
             paint.style = Paint.Style.FILL
             paint.color = Color.parseColor("#7B1FA2")
             canvas.drawCircle(x,y,r,paint)
         }
         fun update(stopcb:(Float,Int)->Unit) {
-
+            state.update(this,stopcb)
         }
         fun startUpdating(holder:CircularMotionButtonHolder,startcb:()->Unit) {
-
+            state.startUpdating(holder,this,startcb)
         }
     }
     data class CircularMotionButtonState(var deg:Float = 0f,var r:Float = 0f,var destJ:Int = 0,var scale:Float = 0f,var dir:Int = 0,var oDeg:Float  =0f) {
-        fun update(stopcb:(Float,Int)->Unit) {
+        fun update(button:CircularMotionButton,stopcb:(Float,Int)->Unit) {
             scale += 0.1f*dir
             deg = oDeg + 180f*scale
             if(scale>1) {
                 scale = 1f
                 dir = 0
                 stopcb(scale,1)
+                deg = oDeg+180f
             }
+
+            button.x = r*Math.cos(deg*Math.PI/180).toFloat()
+            button.y = r*Math.sin(deg*Math.PI/180).toFloat()
         }
         fun startUpdating(holder:CircularMotionButtonHolder,button:CircularMotionButton,startcb:()->Unit) {
-            var r = Math.abs(button.x-holder.x)
+            var r = Math.abs(button.x-holder.x)/2
             if(holder.x > button.x) {
                 deg = 180f
             }
